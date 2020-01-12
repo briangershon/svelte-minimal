50e4622d36cdde0a1d5d1ef0d0a7bc3091f1086d
Brian Gershon <brian.five@gmail.com>
2020-01-11 19:35:58 -0800

    Add Svelte tests.

--- src/specs/hello.spec.js
+++ src/specs/hello.spec.js
@@ -1,6 +1,14 @@
-import getMessage from '../hello';
+import { render } from '@testing-library/svelte';
+import Hello from '../hello.svelte';
 
-test('has default message', () => {
-  const message = getMessage();
-  expect(message).toEqual('Welcome to your Vanilla JavaScript site, built with minimal dependencies.');
+describe('Hello component', () => {
+  test('should render component correctly with defaults', () => {
+    const { container } = render(Hello);
+    expect(container).toContainHTML('<body><div><p>Hello Anonymous!</p></div></body>');
+  });
+
+  test('should render component correctly with "name" prop', () => {
+    const { container } = render(Hello, { props: { name: 'Brian' }});
+    expect(container).toContainHTML('<body><div><p>Hello Brian!</p></div></body>');
+  });
 });
